b1ced824ae3467328167be2fc8feec18db228042
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-04-27 21:38:02 -0700

    Add color to prompt, update files downloaded

--- install.sh
+++ install.sh
@@ -2,23 +2,25 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+CYAN='\033[0;36m'
 NC='\033[0m'
 
 config_auth() {
-  read -p "Enter the Heroku app name that contains the configuration secrets: " HEROKU_APP
+  printf "\n${CYAN}Enter the Heroku app name that contains the configuration secrets: ${NC}"
+  read HEROKU_APP
   if [[ -z ${HEROKU_APP} ]]; then
     return 1
   else
-    echo "Enter a Heroku API key: " 
+    printf "\n${CYAN}Enter the Heroku API key: ${NC}"
     read -s HEROKU_API_KEY
     tokenCheckError=$(curl -s -n https://api.heroku.com/apps/${HEROKU_APP}/config-vars \
       -H "Accept: application/vnd.heroku+json; version=3" \
       -H "Authorization: Bearer ${HEROKU_API_KEY}" | jq -r '.id')
     if [[ ${tokenCheckError} != "null" ]]; then
-      printf "${RED}Unable to authenticate with Heroku, received error '${tokenCheckError}'${NC}\n\n"
+      printf "\n${RED}Unable to authenticate with Heroku, received error '${tokenCheckError}'${NC}\n\n"
       return 1
     else
-      printf "${GREEN}Successfully authenticated with Heroku${NC}\n"
+      printf "\n\n${GREEN}Successfully authenticated with Heroku${NC}\n\n"
       return 0
     fi
   fi
@@ -29,13 +31,15 @@ sudo apt-get install -y jq
 
 until config_auth; do : ; done
 
-curl -O -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/andrewmarklloyd/pi-config/main/app.service.tmpl
+curl -O -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/andrewmarklloyd/pi-config/main/assets/app.service.tmpl
+curl -O -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/andrewmarklloyd/pi-config/main/assets/config-app.sh
 
 # use ~ as a delimiter
-sudo sed -e "s~{{.HEROKU_APP}}~${HEROKU_APP}~" \
+sed -e "s~{{.HEROKU_APP}}~${HEROKU_APP}~" \
     -e "s~{{.HEROKU_API_KEY}}~${HEROKU_API_KEY}~" \
-    app.service.tmpl 
-    > /etc/systemd/system/app.service
+    app.service.tmpl \
+    > app.service
+sudo mv app.service /etc/systemd/system/app.service
 rm app.service.tmpl
 
 sudo systemctl enable app.service
